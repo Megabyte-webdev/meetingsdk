8c61ef5305854327be434dc2f04562a217b1a637
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-06-19 17:40:47 +0100

    new fix

--- dist/index.js
+++ dist/index.js
@@ -39,7 +39,7 @@ var import_react2 = require("react");
 
 // src/config/ws.ts
 var SDK_CONFIG = {
-  wsUrl: "ws://localhost:8080/ws"
+  wsUrl: "wss://rust-video-server-sfyf.onrender.com/ws"
 };
 
 // src/core/MeetingState.ts
