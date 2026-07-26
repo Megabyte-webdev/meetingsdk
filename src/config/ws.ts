49514897dafb19c26f2192ff54865fe9d78db051
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-07-26 22:32:51 +0100

    feat: update WebSocket and base URLs to new server endpoints

--- src/config/ws.ts
+++ src/config/ws.ts
@@ -1,4 +1,4 @@
 export const SDK_CONFIG = {
-  wsUrl: "wss://sfuserver-production.up.railway.app/ws",
-  baseUrl: "https://sfuserver-production.up.railway.app",
+  wsUrl: "wss://https://rust-video-server-sfyf.onrender.com/ws",
+  baseUrl: "https://https://rust-video-server-sfyf.onrender.com",
 };
